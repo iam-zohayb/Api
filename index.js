1f5950961949dd4db0f898b38b4d2862329759b5
iam-zohaib <114738880+iam-zohaib@users.noreply.github.com>
2024-08-14 16:45:56 +0500

    m

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = 4000;
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 });
-
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome, your app is working well');
+  });
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
