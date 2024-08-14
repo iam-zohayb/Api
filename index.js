ee194ce1dfaf1a69a96bba1d3cf492e4ef324e63
iam-zohaib <114738880+iam-zohaib@users.noreply.github.com>
2024-08-14 16:57:28 +0500

    c

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-});
+// Backend/Server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const formRoutes = require('./Routes/FormRoutes');
+const driverRoutes = require('./Routes/DriverRoutes');
+const authRoutes = require('./Routes/authRoutes');
+
+const app = express();
+const PORT =  5000;
+const MONGODB_URI = 'mongodb+srv://Zohaib:Zohaib@cluster0.v7oju4g.mongodb.net/';
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
   });
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+app.use('/api/forms', formRoutes);
+app.use('/api/drivers', driverRoutes); 
+app.use('/api', authRoutes);
 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 module.exports = app;
